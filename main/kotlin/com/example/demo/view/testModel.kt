6dc6323b3c857ec992cb7e48e8a3e43b9c53c063
abdo-kururu <abdo20062012>
2019-01-31 18:10:23 +0200

    commit

--- main/kotlin/com/example/demo/view/testModel.kt
+++ main/kotlin/com/example/demo/view/testModel.kt
@@ -1,103 +1,75 @@
 package com.example.demo.view
-
 import javafx.beans.property.SimpleStringProperty
-import javafx.collections.FXCollections
-import javafx.collections.ObservableList
-import javafx.scene.Scene
-import javafx.scene.paint.Color
+import javafx.scene.control.Alert
+import javafx.scene.control.Button
+import javafx.scene.control.ProgressIndicator
 import tornadofx.*
-import javax.json.JsonObject
-import javax.swing.text.html.ListView
+import tornadofx.getValue
+import tornadofx.setValue
 
-class testModel : View("My View") {
-    init {
-importStylesheet("/ex.css")
-    }
-    var cont=CustomerController()
-    var ls:javafx.scene.control.ListView<cust>?=null
-    var ts=TaskStatus()
-override val root=vbox{
-    button("load custmers")
-    {
-        setOnAction {
-            refresh()
-        }
-    }
-        listview<cust> {
-            ls=this
-            prefHeight=150.0
-            style{
-                backgroundColor+= Color.AZURE
-            }
-        }
+class testModel : View("login"){
+    var usr=user()
+    var model=userModel(usr)
+    var g =githup()
+    override val root=form{
 
-        vbox(4.0){
-            progressbar(ts.progress)
-            label(ts.message)
-            visibleWhen { ts.running }
-        }
+fieldset {
+    field("name") { textfield (model.name){  } }
+    field("password") {passwordfield (model.pass){  }  }
 
-}
-
-    fun refresh(){
-        runAsync(ts) {
-            updateProgress(.4,1.0)
-            updateMessage("load customers")
-            cont.loadCustomers()
-        }ui{
-            ls?.items=cont.loadCustomers()
+    button("login") {
+useMaxSize=true
+        action {
+            login()
         }
     }
 }
 
-class CustomerController : Controller() {
-    val api: Rest by inject()
-    init {
-        api.baseURI = "https://api.github.com/users/hadley/orgs"
     }
-    fun loadCustomers():ObservableList<cust>
-    {
-        return api.get("login").list().toModel<cust>().toList().observable()
+fun Button.login(){
+if(model.commit()){
+    graphic= ProgressIndicator()
+
+    runAsync {
+        g.login(model.use)
+
+    }ui{sucess->
+        graphic=null
+
+        if (sucess)
+        alert(Alert.AlertType.INFORMATION,content = "sucess",header = "success")
+else
+        alert(Alert.AlertType.INFORMATION,content = "faile",header = "failed")
+
     }
-
-
-
 }
-class cust:JsonModel{
-var nameProperty= FXCollections.observableArrayList<names>()
-    override fun updateModel(json: JsonObject) {
-        with(json)
-        {
-nameProperty.setAll(getJsonArray("login").toModel())
-        }
-    }
-
-    override fun toJSON(json: JsonBuilder) {
-
-        with(json){
-            add("nameProperty", nameProperty.toJSON())
-        }
-    }
-
-
+}
 
 }
 
-class names : JsonModel {
+class user {
+    val passProperty = SimpleStringProperty()
+    var pass by passProperty
 
-
-    var nameProperty=SimpleStringProperty()
+    val nameProperty = SimpleStringProperty()
     var name by nameProperty
+}
+ class userModel(var use:user):ViewModel(){
+     var name =bind{use.nameProperty}
+var pass=bind{use.passProperty}
 
-    override fun updateModel(json: JsonObject) {
-        with(json) {
-            name = string("name")
-        }
-    }
+ }
 
-    override fun toJSON(json: JsonBuilder) {
-        with(json) {
-            add("name", name)
-        }
+
+class githup:Controller(){
+    val api:Rest by inject()
+init {
+    api.baseURI="https://api.github.com"
+}
+    fun login(us:user):Boolean{
+
+        api.setBasicAuth(us.nameProperty.value, us.passProperty.value)
+
+        return api.get("user").consume().ok()
     }
 }
